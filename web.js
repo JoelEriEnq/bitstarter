e57b3d2f3ba6bad352ae83d81a75e230c8a2b252
JoelEriEnq <joel.eriksson@creandum.com>
2013-07-14 16:57:01 +0000

    Reading from index.html2.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     var fs = require('fs');
 
-    var buffer = fs.readFileSync('index.html','utf8');
+    var buffer = new Bbuffer(fs.readFileSync('index.html','utf8'));
 
   response.send(buffer.toString())
 )});
