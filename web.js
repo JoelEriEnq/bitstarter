a1e993f8b5532a8d97f56367ddfdf06000af47c4
JoelEriEnq <joel.eriksson@creandum.com>
2013-07-14 16:05:33 +0000

    Reading from index.html.

--- web.js
+++ web.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+    var fs = require('fs');
+
+    var buffer = fs.readFileSync('index.html','utf8');
+
+  response.send(buffer.toString())
+)});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
